b817f2061497ee53498eb3aee85d30aec4316bfb
Kayla Luebbert <luebbertkayla@gmail.com>
2021-06-08 00:29:47 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -42,7 +42,8 @@ class Dog
   end 
   
   def self.new_from_db(row)
-    id = row[0]
+    attributes_hash = {
+      id = row[0]
     name = row[1]
     breed = row[2]
     self.new(id, name, breed)
