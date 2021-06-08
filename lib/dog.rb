699e4c3e06c7b8e6a3c01566b9392a8227a280ab
Kayla Luebbert <luebbertkayla@gmail.com>
2021-06-08 01:06:15 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -70,7 +70,7 @@ class Dog
     end.first
   end 
   
-  def find_or_create_by(name:, breed:)
+  def find_or_create_by(name, breed)
     sql = <<-SQL 
       SELECT * FROM dogs WHERE name = ? AND breed = ?
     SQL
@@ -85,8 +85,6 @@ class Dog
     new_dog
   end 
       
-    
-  
   def update
     sql = <<-SQL
       UPDATE dogs SET name = ?, breed = ? WHERE id =?
