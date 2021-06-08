f1637ad56fba0d2f83822fec076532499125403b
Kayla Luebbert <luebbertkayla@gmail.com>
2021-06-08 00:57:58 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -71,7 +71,10 @@ class Dog
   end 
   
   def find_or_create_by(name:, breed:)
-    dog = DB[:conn].execute("SELECT * FROM dogs WHERE name = ? AND breed = ?", name, breed)
+    sql = <<-SQL 
+      SELECT * FROM dogs WHERE name = ? AND breed = ?
+    SQL
+      
     
   
   def update
