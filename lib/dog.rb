ef5de50d0a155053af51d980dc77e282884fc13f
Kayla Luebbert <luebbertkayla@gmail.com>
2021-06-08 00:45:29 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -63,4 +63,9 @@ class Dog
   def update
     sql = <<-SQL
       UPDATE dogs SET name = ?, breed =? WHERE id =?
+      SQL
+      
+      DB[:conn].execute(sql, self.name, self.breed, self.id)
+  end
+
 end 
