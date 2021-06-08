465b7872e399bbc8bd50b220a958006b8ffa667a
Kayla Luebbert <luebbertkayla@gmail.com>
2021-06-08 01:08:39 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -70,22 +70,20 @@ class Dog
     end.first
   end 
   
-  def self.find_or_create_by(name:, breed:)
-    sql = <<-SQL
-      SELECT * FROM dogs
-      WHERE name = ? AND breed = ?
-      SQL
-
-
-      dog = DB[:conn].execute(sql, name, breed).first
-
-      if dog
-        new_dog = self.new_from_db(dog)
-      else
-        new_dog = self.create({:name => name, :breed => breed})
-      end
-      new_dog
-  end
+  def find_or_create_by(name:, breed:)
+    sql = <<-SQL 
+      SELECT * FROM dogs WHERE name = ? AND breed = ?
+    SQL
+    
+    dog = DB[:conn].execute(sql, name, breed).first
+    
+    if dog 
+      new_dog = self.new_from_db(row)
+    else 
+      new_dog = self.create({:name => name, :breed => breed})
+    end 
+    new_dog
+  end 
       
   def update
     sql = <<-SQL
